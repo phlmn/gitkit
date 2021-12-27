9b7cdd34be8ea07fa3bf8526396d017e88d83830
Philipp Mandler <info@philipp-mandler.com>
2021-12-27 14:56:57 +0100

    git: Add CloneRepo

--- git.go
+++ git.go
@@ -20,6 +20,20 @@ func InitRepo(name string, config *Config) error {
 	return nil
 }
 
+func CloneRepo(name string, config *Config, url string) error {
+	fullPath := path.Join(config.Dir, name)
+
+	if err := exec.Command(config.GitPath, "clone", "--bare", url, fullPath).Run(); err != nil {
+		return err
+	}
+
+	if config.AutoHooks && config.Hooks != nil {
+		return config.Hooks.setupInDir(fullPath)
+	}
+
+	return nil
+}
+
 func RepoExists(p string) bool {
 	_, err := os.Stat(path.Join(p, "objects"))
 	return err == nil
